b89cf0ca59a48536cfc34dd162da179d7e4aaccc
Patrick Copp <pcopp@uoguelph.ca>
2021-01-20 00:40:48 -0500

    Quote is now returned with words around it, somewhat scuffed but a good addition and working for now

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const port = 8000;
 var cors = require('cors')
 const path = require('path')
 var json = require('./data_file.json');
+const { maxHeaderSize } = require('http');
 app.use(cors())
 
 app.get('/', (req, res) => {
   console.log(req.query.quote);
   let toReturn = [];
   json.forEach(element => {
-    if(element["script"].toLowerCase().includes(req.query.quote.toLowerCase()))
+    index = element["script"].toLowerCase().indexOf(req.query.quote.toLowerCase());
+    if(index!=-1)
     {
       toReturn.push({
         title:element["title"].substring(0,element["title"].length-4),
-        script:"THIS IS THE SCRIPT",
+        script:lastCoupleWords(element["script"],index),
         time:timeCalc(req.query.quote,element["script"])
       });
     }
@@ -34,4 +36,23 @@ function timeCalc(quote,script)
   minutes = parseInt(seconds / 60);
   seconds = seconds % 60;
   return minutes+":"+("0" + seconds).slice(-2);
+}
+
+function lastCoupleWords(script,index)
+{
+  let startIndex = Math.max(0,index-100);
+  let endIndex = Math.min(script.length-1,index+100);
+  while (script[startIndex]!= '.')
+  {
+    //console.log(script[startIndex])
+    startIndex++;
+  }
+  startIndex++;
+  while (script[endIndex]!= '.')
+  {
+    endIndex--;
+  }
+  endIndex++;
+
+  return script.substring(Math.min(startIndex,index),Math.max(endIndex));
 }
